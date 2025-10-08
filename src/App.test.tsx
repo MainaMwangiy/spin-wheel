8d387d4b2997143c8f3a6a1b2bd16483859c92b2
MainaMwangiy <charlesmaina8848@gmail.com>
2025-10-08 15:53:30 +0300

    feat: code cleanup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
-import { render } from '@testing-library/react';
-import { screen } from '@testing-library/dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
